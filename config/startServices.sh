1896bd0e010f9ef306fe9f95dd6e5c233f06a826
Jason Ziemba <jason@ziemba.net>
2022-06-07 07:36:57 -0400

    Tweaking out bugs.

--- config/startServices.sh
+++ config/startServices.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Forgot, pipeline doesn't like installing some stuff, installing here
+apt update
+apt install -y openssh-server
+
 cd /opt/acme/acme.sh
 # Using --force so it'll install without cron being installed
 echo $sslUser
