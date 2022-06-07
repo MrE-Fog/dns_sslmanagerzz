19b7e0962adba36102dbe2f1ae757affe7ed8ff2
Jason Ziemba <jason@ziemba.net>
2022-06-07 10:10:17 -0400

    Pivoting to perform live runs

--- config/startServices.sh
+++ config/startServices.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # Forgot, pipeline doesn't like installing some stuff, installing here
-apt update
-apt install -y openssh-server
+# *** Commenting out as testing is no longer active ***
+# apt update
+# apt install -y openssh-server
 
 cd /opt/acme/acme.sh
 # Using --force so it'll install without cron being installed
 echo $sslUser
 ./acme.sh --install --force -m $sslUser
 
+# *** Commenting out as testing is no longer active ***
+# mkdir /run/sshd
+# /usr/sbin/sshd -D
 
-mkdir /run/sshd
-/usr/sbin/sshd -D
+time /opt/sslManager -verbose
