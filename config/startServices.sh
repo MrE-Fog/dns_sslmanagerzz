a5f3ef60f33abdf5ae0e87de6c90e24d25a3c182
Jason Ziemba <jason@ziemba.net>
2022-06-07 10:25:31 -0400

    Removing cron (for now) and updating startup process

--- config/startServices.sh
+++ config/startServices.sh
@@ -2,16 +2,17 @@
 
 # Forgot, pipeline doesn't like installing some stuff, installing here
 # *** Commenting out as testing is no longer active ***
-# apt update
-# apt install -y openssh-server
+apt update
+apt install -y openssh-server
 
 cd /opt/acme/acme.sh
 # Using --force so it'll install without cron being installed
 echo $sslUser
 ./acme.sh --install --force -m $sslUser
 
-# *** Commenting out as testing is no longer active ***
-# mkdir /run/sshd
-# /usr/sbin/sshd -D
-
 time /opt/sslManager -verbose -debug
+
+# *** Commenting out as testing is no longer active ***
+# Start SSH after script exits to allow system access
+mkdir /run/sshd
+/usr/sbin/sshd -D
